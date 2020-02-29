b0d3b595d2c2371ab362411eb85f697245d36132
irwin <84185456@qq.com>
2020-03-01 02:44:45 +0800

    D01 基于SOA搭建基础框架程序

---
+++ ClientConsole/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceReference1.Service1Client client = new ServiceReference1.Service1Client();
+            var result = client.GetData(100);
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ WcfHostConsoleApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using WcfServiceLib;
+
+namespace WcfHostConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            ServiceHost serviceHost = new ServiceHost(typeof(Service1));
+            serviceHost.Open();
+            Console.WriteLine("Wcf服务已启动。");
+            Console.ReadLine();
+            serviceHost.Close();
+        }
+    }
+}
